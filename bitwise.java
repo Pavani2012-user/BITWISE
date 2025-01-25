8c04a9bcc122cfa73a259701dbc2e5def76002f6
Pavani2012-user <pavanibattu006@gmail.com>
2025-01-25 12:48:03 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,10 @@
+public class bitwise {
+        public static void main(String[] args) {
+            int num = 8;
+            System.out.println("Original: " + num);
+            System.out.println("Left Shift (num << 1): " + (num << 1)); // 16
+            System.out.println("Right Shift (num >> 1): " + (num >> 1));
+        }
+    }
+
+    
